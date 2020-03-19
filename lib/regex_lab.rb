92cc3c8df67dff8f20d67630f28c6a13288a525c
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-03-19 21:31:07 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -4,8 +4,8 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  result = text.match(/^un\w+ing$/)
-  result ? true : false
+  result = text.scan(/^un\w+ing$/)
+  result
 end
 
 def words_five_letters_long(text)

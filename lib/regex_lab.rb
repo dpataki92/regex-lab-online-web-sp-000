d48e43b78ccb568e2ae3e5bf268df0cbf8d40abc
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-03-19 22:05:29 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -14,7 +14,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  result = text.match(/[A-Z]\w+\W/)
+  result = text.match(/\b[A-Z]\w+\W\b/)
   result ? true : false
 end
 

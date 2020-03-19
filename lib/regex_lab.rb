dbafe143415d67332944dddd5eb21e87396270be
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-03-19 22:07:52 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -14,7 +14,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  result = text.match(/\b[A-Z]\.+\W\b/)
+  result = text.match(/[A-Z].\W/)
   result ? true : false
 end
 
